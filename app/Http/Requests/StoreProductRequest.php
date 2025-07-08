4b1f55dd608aab4c62366584c9a62994e7309012
Augusto Rickes <ag.augusto.rickes@gmail.com>
2025-07-08 08:53:26 -0300

    feat: criacao do formrequest para os produtos

---
+++ app/Http/Requests/StoreProductRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255|unique:products,name',
+            'price' => 'required|numeric|min:0|max:999999.99'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'O nome do produto é obrigatório.',
+            'name.unique' => 'Já existe um produto com este nome.',
+            'name.max' => 'O nome do produto não pode ter mais de 255 caracteres.',
+            'price.required' => 'O preço do produto é obrigatório.',
+            'price.numeric' => 'O preço deve ser um valor numérico.',
+            'price.min' => 'O preço deve ser maior ou igual a zero.',
+            'price.max' => 'O preço não pode ser maior que R$ 999.999,99.'
+        ];
+    }
+}
