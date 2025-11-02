61ca88a4f099c7f03a51e8d11ea62273c50b05dc
augusto rickes da silveira <ag.augusto.rickes@gmail.com>
2025-11-02 18:56:52 -0300

    feat: Refactor product management routes and enhance product controller functionality

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
@@ -12,20 +13,29 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
+        // Pegar o usuário teste para associar os produtos
+        $user = User::where('email', 'test@example.com')->first();
+        
+        if (!$user) {
+            return;
+        }
+
         Product::create([
+            'user_id' => $user->id,
             'name' => 'Melancia pequena',
             'price' => 15
         ]);
 
         Product::create([
+            'user_id' => $user->id,
             'name' => 'Melancia média',
             'price' => 20
         ]);
 
         Product::create([
+            'user_id' => $user->id,
             'name' => 'Melancia grande',
             'price' => 30
         ]);
-
     }
 }
